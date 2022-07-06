dfe912eb168aaf56f76735388b7a76f2f247b8f8
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-07-06 09:35:53 -0300

    Iniciando o TodoHandler

---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,25 @@
+using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            command.Validate();
+            if (command.Invalid)
+                return new GenericComandResult(false, "Seu comando está invalido", command.Notifications);
+        }
+    }
+}
