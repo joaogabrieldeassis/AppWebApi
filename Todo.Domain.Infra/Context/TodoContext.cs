e3822e7ef678868fd3fdf0ea03d9e5b8cffe2009
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-07-07 15:05:10 -0300

    Iniciando os mapeamentos com o entity Framework

---
+++ Todo.Domain.Infra/Context/TodoContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Infra.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().Property(x => x.Id);
+            modelBuilder.Entity<TodoItem>().Property(x => x.User).HasMaxLength(120).HasColumnName("varchar(120)");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Title).HasMaxLength(160).HasColumnName("varchar(160)");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Done).HasColumnType("bit");
+            modelBuilder.Entity<TodoItem>().HasIndex(X => X.User);
+        }
+    }
+}
