357ed13ff781bc17641ba03a1e896157c32ed719
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-07-07 13:45:30 -0300

    Atualizando um TodoItem

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -8,7 +8,8 @@ using Todo.Domain.Repositories;
 namespace Todo.Domain.Handlers
 {
     public class TodoHandler : Notifiable,
-        IHandler<CreateTodoCommand>
+        IHandler<CreateTodoCommand>,
+        IHandler<UpdateTodoCommand>
     {
         private readonly ITodoRepository _repository;
 
@@ -29,6 +30,25 @@ namespace Todo.Domain.Handlers
             //Salva no banco
             _repository.Create(todo);
             return new GenericCommandResult(true, "Tarefa salva", todo);
+        }
+
+        public ICommandResult Handle(UpdateTodoCommand command)
+        {
+            //Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "A atualização não pode ser concluida", command.Notifications);
+
+            //Recupera o usuario
+            var todo = _repository.GetById(command.Id, command.User);
+
+            //Altera o usuario 
+            todo.UpdateTitle(command.Title);
+
+            //salva no banco
+            _repository.Update(todo);
+
+            return new GenericCommandResult(true, "As alterações foram salvas com sucesso", command.Notifications);
         }
     }
 }
