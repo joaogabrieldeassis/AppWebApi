7015d57412088111e96fa9d1fd24cf2d1ed1cf8b
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-07-07 13:20:26 -0300

    Teste do todo finalizado

--- Todo.Domain.Tests/EntityTests/TodoItemTests.cs
+++ Todo.Domain.Tests/EntityTests/TodoItemTests.cs
@@ -1,23 +1,21 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Todo.Domain.Commands;
+using Todo.Domain.Entities;
 
 namespace Todo.Domain.Tests.CommandTests
 {
     [TestClass]
     public class TodoItemTests
     {
+        private readonly TodoItem _todo = new TodoItem("Tarefa", "joao", DateTime.Now);
 
         [TestMethod]
-        public void Dado_Um_Comando_Invalido()
+        public void Dado_Um_Novo_Todo_O_Mesmo_Nao_Pode_Ser_Concluido()
         {
-            Assert.Fail();
+            Assert.AreEqual(_todo.Done, false);
         }
 
-        [TestMethod]
-        public void Dado_Um_Comando_Valido()
-        {
-            Assert.Fail();
-        }
+
     }
 }
