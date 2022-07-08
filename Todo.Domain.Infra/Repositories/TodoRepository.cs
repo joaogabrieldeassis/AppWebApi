e65210c00a1d079be42c88c51b6f99813cd4d583
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-07-08 15:20:41 -0300

    Iniciando o crontoller da api

--- Todo.Domain.Infra/Repositories/TodoRepository.cs
+++ Todo.Domain.Infra/Repositories/TodoRepository.cs
@@ -26,41 +26,27 @@ namespace Todo.Domain.Infra.Repositories
 
         public IEnumerable<TodoItem> GetAll(string user)
         {
-            return _context.Todos
-               .AsNoTracking()
-               .Where(TodoQueries.GetAll(user))
-               .OrderBy(x => x.Date);
+            return _context.Todos.AsNoTracking().Where(TodoQueries.GetAll(user)).OrderBy(x => x.Date);
         }
 
         public IEnumerable<TodoItem> GetAllDone(string user)
         {
-            return _context.Todos
-                .AsNoTracking()
-                .Where(TodoQueries.GetAllDone(user))
-                .OrderBy(x => x.Date);
+            return _context.Todos.AsNoTracking().Where(TodoQueries.GetAllDone(user)).OrderBy(x => x.Date);
         }
 
         public IEnumerable<TodoItem> GetAllUndone(string user)
         {
-            return _context.Todos
-                .AsNoTracking()
-                .Where(TodoQueries.GetAllUndone(user))
-                .OrderBy(x => x.Date);
+            return _context.Todos.AsNoTracking().Where(TodoQueries.GetAllUndone(user)).OrderBy(x => x.Date);
         }
 
         public TodoItem GetById(Guid id, string user)
         {
-            return _context
-                .Todos
-                .FirstOrDefault(x => x.Id == id && x.User == user);
+            return _context.Todos.FirstOrDefault(x => x.Id == id && x.User == user);
         }
 
         public IEnumerable<TodoItem> GetByPeriod(string user, DateTime date, bool done)
         {
-            return _context.Todos
-                .AsNoTracking()
-                .Where(TodoQueries.GetByPeriod(user, date, done))
-                .OrderBy(x => x.Date);
+            return _context.Todos.AsNoTracking().Where(TodoQueries.GetByPeriod(user, date, done)).OrderBy(x => x.Date);
         }
 
         public void Update(TodoItem todo)
